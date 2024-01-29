56cb7dff98cfe1eab5000c7bea1f69abd57d735a
DeKarLo <karen.ananyan2017@mail.ru>
2024-01-29 17:09:31 +0600

    debug

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -33,6 +33,7 @@ func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	dsn := flag.String("dsn", "web:123455@/snippetbox?parseTime=true", "MySQLdata source name")
 	debug := flag.Bool("debug", false, "Enable debug mode")
+
 	flag.Parse()
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t",
@@ -56,6 +57,7 @@ func main() {
 	sessionManager.Cookie.Secure = true
 
 	app := &application{
+		debug:          *debug,
 		errorLog:       errorLog,
 		infoLog:        infoLog,
 		snippets:       &models.SnippetModel{DB: db},
